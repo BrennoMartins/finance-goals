9b3ce2f80e44a4d6ce4279623efafa9001e080e5
Brenno Martins <brennomartins@hotmail.com>
2026-04-03 11:55:31 -0300

    Add initial project structure and configuration files

---
+++ test/finance_goals/core_test.clj
@@ -0,0 +1,7 @@
+(ns finance-goals.core-test
+  (:require [clojure.test :refer :all]
+            [finance-goals.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
