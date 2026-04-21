d0cb9d9d86733b79fb495d81a32b0e5821537570
Brenno Martins <brennomartins@hotmail.com>
2026-04-21 12:24:30 -0300

    Fix return type of get-all-macro-goals function to return a list of macro goals

--- src/finance_goals/controller/macro_goals.clj
+++ src/finance_goals/controller/macro_goals.clj
@@ -4,8 +4,10 @@
             [finance-goals.diplomatic.db.macro-goals :as db.macro-goals]
             [schema.core :as s]))
 
-(s/defn get-all-macro-goals :- model.macro-goal/macro-goal
+
+; TODO teminar o CRUD
+(s/defn get-all-macro-goals :- [model.macro-goal/macro-goal]
   []
-  (let [db (db.finance-db/db)
-        table :macro_goals]
+  (let [db db.finance-db/db
+        table "macro_goals"]
     (db.macro-goals/return-all-macro-goals db table)))
